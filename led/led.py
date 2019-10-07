9248bce62c289d02d7dbd097d0bc37c031d7a830
Nazmus Nasir <nazmus.nasir@gmail.com>
2019-10-06 21:06:38 -0400

    added status file asjson

--- led/led.py
+++ led/led.py
@@ -11,14 +11,14 @@ GPIO.setup(18, GPIO.OUT)
 @app.route('/', methods=['GET'])
 def led():
     status = request.args.get('status')
-    f = open("status.txt", "w");
+    f = open("status.json", "w");
     if status == "on":
         GPIO.output(18, GPIO.HIGH)
-        f.write(1);
+        f.write(jsonify({"status": "on"}));
         return jsonify({"message": "Led successfully turned on"})
     elif status == "off":
         GPIO.output(18, GPIO.LOW)
-        f.write(0);
+        f.write(jsonify({"status": "off"}));
         return jsonify({"message": "Led successfully turned off"})
     else:
         return jsonify({"message": "Not a valid status"})
