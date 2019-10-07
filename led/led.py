78d7a2ebb6afba743396b28d1921ba00ee9b1741
Nazmus Nasir <nazmus.nasir@gmail.com>
2019-10-07 00:54:25 -0400

    typecast write to str

--- led/led.py
+++ led/led.py
@@ -15,12 +15,12 @@ def led():
     if status == "on":
         GPIO.output(18, GPIO.HIGH)
         with open("status.txt", "w") as f:
-            f.write(str("1"))
+            f.write("on")
         return jsonify({"message": "Led successfully turned on"})
     elif status == "off":
         GPIO.output(18, GPIO.LOW)
         with open("status.txt", "w") as f:
-            f.write(str("0"))
+            f.write("off")
         return jsonify({"message": "Led successfully turned off"})
     else:
         return jsonify({"message": "Not a valid status"})
