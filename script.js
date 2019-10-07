f19fd65393bd7a805e121178e098f13a110790d2
Nazmus Nasir <nazmus.nasir@gmail.com>
2019-10-07 02:28:26 -0400

    updated UI

--- script.js
+++ script.js
@@ -1,54 +1,84 @@
 $(document).ready(function() {
+    let status = 0;
+
     $.ajax({
         url: 'led/status.txt',
         method: 'GET',
         dataType: 'text',
         success: function (result) {
             console.log(result);
+            status = result;
+            btnStatus();
+        //     if(result == 0) {
+        //     $(this).text('Turn Off')
+        //     $(this).removeClass().addClass('btn btn-block btn-light');
+        //     status = 'on';
+        // } else {
+        //     $(this).text('Turn On');
+        //     $(this).removeClass().addClass('btn btn-block btn-dark');
+        //     status = 'off';
+        // }
         }
     });
 
-    $('#turnOnBtn').on('click', function(e){
-        $.ajax({
-            url: '/api/kitchen?status=on',
-            method: 'GET',
-            success: function(result) {
-                console.log(result);
-         }
-        });
-        e.preventDefault();
-    });
-    
-    $('#turnOffBtn').on('click', function(e){
-        $.ajax({
-            url: '/api/kitchen?status=off',
-            method: 'GET',
-            success: function(result) {
-                console.log(result);
-         }
-        });
-        e.preventDefault();
-    });
+    // $('#turnOnBtn').on('click', function(e){
+    //     $.ajax({
+    //         url: '/api/kitchen?status=on',
+    //         method: 'GET',
+    //         success: function(result) {
+    //             console.log(result);
+    //      }
+    //     });
+    //     e.preventDefault();
+    // });
+    //
+    // $('#turnOffBtn').on('click', function(e){
+    //     $.ajax({
+    //         url: '/api/kitchen?status=off',
+    //         method: 'GET',
+    //         success: function(result) {
+    //             console.log(result);
+    //      }
+    //     });
+    //     e.preventDefault();
+    // });
     
     $('#btnToggle').on('click', function(e){
-        let status;
-        if($(this).text() == 'Turn On') {
-            $(this).text('Turn Off')
-            $(this).removeClass().addClass('btn btn-block btn-light');
-            status = 'on';
+        // let status;
+        // if($(this).text() == 'Turn On') {
+        //     $(this).text('Turn Off')
+        //     $(this).removeClass().addClass('btn btn-block btn-light');
+        //     status = 'on';
+        // } else {
+        //     $(this).text('Turn On');
+        //     $(this).removeClass().addClass('btn btn-block btn-dark');
+        //     status = 'off';
+        // }
+        let state;
+        if(status == 0) {
+            state = 'on';
+            status = 1;
         } else {
-            $(this).text('Turn On');
-            $(this).removeClass().addClass('btn btn-block btn-dark');
-            status = 'off';
+            state = 'off'
+            status = 0;
         }
-        
         $.ajax({
-            url: '/api/kitchen?status=' + status,
+            url: '/api/kitchen?status=' + state,
             method: 'GET',
             success: function(result) {
                 console.log(result);
-         }
+         }, complete: btnStatus
         });
         e.preventDefault();
     });
+
+    function btnStatus() {
+        if(status == 0) {
+            $(this).text('Turn On');
+            $(this).removeClass().addClass('btn btn-block btn-dark');
+        } else {
+            $(this).text('Turn Off')
+            $(this).removeClass().addClass('btn btn-block btn-light');
+        }
+    }
 });
